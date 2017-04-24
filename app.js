126fdf15963198067ef7268803d736f89c99050a
Jason Oscar <jhoscar@gmail.com>
2017-04-24 18:33:37 -0400

    Adding volleyball setting up logging.

--- app.js
+++ app.js
@@ -1,13 +1,31 @@
 const express = require('express');
+const volleyball = require('volleyball');
 var app = express();
 
-app.get('/', function(req, res) {
-    res.send('Hey there');
+app.use(volleyball);
+
+app.use('/', function(req, res, next) {
+    res.status(200);
+    next();
+});
+
+app.get('/', function(req, res, next) {
+    res.send('Hey there\n');
+    next();
 });
 
 app.get('/news', function(req, res) {
     res.send('Extra extra read all about it');
 });
+
+app.get('/special/', function(req, res, next) {
+    res.send('You reached the special area.\n');
+    next();
+})
+
+// app.use(function(req, res, next) {
+//     console.log(req.method, req.url, res.statusCode);
+// });
 
 
 app.listen(3000, function() {
