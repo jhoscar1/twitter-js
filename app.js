b41d80b38f867ace983471504329e32cf6453260
Jason Oscar <jhoscar@gmail.com>
2017-04-25 11:00:37 -0400

    set up routing and stylesheets

--- app.js
+++ app.js
@@ -3,30 +3,22 @@ const volleyball = require('volleyball');
 const nunjucks = require('nunjucks');
 var env = nunjucks.configure('./views', { noCache: true });
 var app = express();
+const routes = require('./routes');
+
+app.use('/', routes);
 
 app.set('view engine', 'html');
 app.engine('html', nunjucks.render);
 
 app.use(volleyball);
 
+app.use(express.static(__dirname + '/public'))
+
 app.use('/', function(req, res, next) {
     res.status(200);
     next();
 });
 
-app.get('/', function(req, res, next) {
-    res.render('index.html', {title: 'An Example', people: [{name: 'Gandalf'}, {name: 'Frodo'}, {name: 'Hermione'}]});
-    next();
-});
-
-app.get('/news', function(req, res) {
-    res.send('Extra extra read all about it');
-});
-
-app.get('/special/', function(req, res, next) {
-    res.send('You reached the special area.\n');
-    next();
-})
 
 nunjucks.render('index.html', {title: 'An Example', people: {
     wizard: {
